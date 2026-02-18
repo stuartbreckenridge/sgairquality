7f3776d21a4b400bd9b911f150309bea1f70dea6
Stuart Breckenridge <stuart.breckenridge@icloud.com>
2026-02-18 10:05:41 +0800

    Initial Commit

---
+++ sgairquality/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sgairquality
+//
+//  Created by Stuart Breckenridge on 18/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sgairquality/sgairqualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  sgairqualityApp.swift
+//  sgairquality
+//
+//  Created by Stuart Breckenridge on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct sgairqualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
